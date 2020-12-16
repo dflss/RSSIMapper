5740b30b10a749322ce724bb12b9f7f1b19cc2ce
Dominika Florczykowska <dflorczy@protonmail.com>
2020-12-16 12:27:41 +0000

    Add measurement functionality

---
+++ measurements.py
@@ -0,0 +1,30 @@
+import statistics
+import time
+
+
+class Measurements:
+    def __init__(self, serial_conn, points_number, timeout):
+        self.serial_conn = serial_conn
+        self.points_number = points_number
+        self.timeout = timeout
+
+    def measure_point(self):
+        rssi_values = []
+        start = time.time()
+        while time.time() - start < self.timeout and len(rssi_values) < self.points_number:
+            bytes_to_read = self.serial_conn.ser.inWaiting()
+            text_read = self.serial_conn.ser.read(bytes_to_read).decode(encoding='utf-8')
+            if text_read:
+                args = text_read.split(" ")
+                for arg in args:
+                    if "RSSI" in arg:
+                        rssi = arg.split("=")[1].strip()
+                        try:
+                            rssi_values.append(int(rssi))
+                        except Exception as ex:
+                            print(ex)
+            time.sleep(0.1)
+        try:
+            return statistics.mean(rssi_values), (len(rssi_values)/self.points_number)*100
+        except statistics.StatisticsError:
+            return None
