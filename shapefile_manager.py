c25257d2f8c683eb3ca3093a5ba9da008da8e515
dflss <dflorczy@protonmail.com>
2020-12-30 12:02:21 +0000

    feat: add polygon shapefile creation (#19)
    closes #18

--- shapefile_manager.py
+++ shapefile_manager.py
@@ -2,7 +2,7 @@ from typing import List
 
 import matplotlib.pyplot as plt
 import shapefile as shp  # type: ignore
-import seaborn as sns  # type: ignore
+# import seaborn as sns  # type: ignore
 import pandas as pd
 
 
@@ -10,27 +10,27 @@ class ShapefileManager:
 
     def write(self, csv: str, shapefile: str):
         data = pd.read_csv(csv)
-        with shp.Writer(shapefile) as w:
-            w.field('X', 'F', 10, 8)
-            w.field('Y', 'F', 10, 8)
+        with shp.Writer(shapefile, shapeType=shp.POLYGON) as w:
             w.field('ID', 'N')
-            for i, point in data.iterrows():
-                w.point(point['x'], point['y'])
-                w.record(point['x'], point['y'], point['id'])
+            for i, polygon in data.iterrows():
+                w.poly([[
+                    [polygon['x1'], polygon['y1']],
+                    [polygon['x2'], polygon['y2']],
+                    [polygon['x3'], polygon['y3']],
+                    [polygon['x4'], polygon['y4']],
+                    [polygon['x1'], polygon['y1']],
+                ]])
+                w.record(polygon['id'])
 
     def read(self, filename: str) -> List[int]:
         sf = shp.Reader(filename)
-        x_values = []
-        y_values = []
         ids = []
+        plt.figure()
         for shape in sf.shapeRecords():
-            x = shape.shape.points[0][0]
-            y = shape.shape.points[0][1]
-            x_values.append(x)
-            y_values.append(y)
+            x = [i[0] for i in shape.shape.points[:]]
+            y = [i[1] for i in shape.shape.points[:]]
+            plt.plot(x, y)
         for rec in sf.records():
             ids.append(rec['ID'])
-        data = pd.DataFrame({'x': x_values, 'y': y_values})
-        sns.scatterplot(x="x", y="y", data=data, palette="coolwarm")
         plt.show()
         return ids
