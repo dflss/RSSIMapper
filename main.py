feceabfac6c8e87351b4705d37ffd914cf98a873
Dominika Florczykowska <dflorczy@protonmail.com>
2020-12-31 18:45:45 +0000

    Refactor

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import argparse
 from typing import List
 
+import shapefile_manager as shapefile_mgr
 from measurements import Measurements
 from program_data import ProgramData
 from serial_connection import SerialConnection
-from shapefile_manager import ShapefileManager
 
 
 def parse_cmd_args() -> ProgramData:
@@ -60,38 +60,26 @@ def perform_measurements(program_data: ProgramData, ids: List[int]):
                 result = measurements.measure_point()
                 if result:
                     rssi, perc = result
-                    shapefile_man = ShapefileManager()
-                    shapefile_man.update_map_with_rssi_data(program_data.output_shapefile, mp_id, int(rssi), int(perc))
+                    shapefile_mgr.update_output_shapefile_map_with_rssi_values(
+                        program_data.output_shapefile, mp_id, int(rssi), int(perc)
+                    )
             else:
                 print("This id does not exist in the given shapefile.")
         except ValueError:
             print("Measurement point id must be an integer value.")
 
 
-def create_shapefile(program_data: ProgramData, shapefile_man: ShapefileManager):
-    shapefile_man.write_input_map(program_data.input_csv, program_data.input_shapefile)
-
-
-def read_input_shapefile(program_data: ProgramData, shapefile_man: ShapefileManager) -> List[int]:
-    return shapefile_man.read_input(program_data.input_shapefile)
-
-
-def read_output_shapefile(program_data: ProgramData, shapefile_man: ShapefileManager) -> List[int]:
-    return shapefile_man.read_output_rssi(program_data.output_shapefile)
-
-
 def main():
     program_data = parse_cmd_args()
-    shapefile_man = ShapefileManager()
     if program_data.input_csv:
-        create_shapefile(program_data, shapefile_man)
-    shapefile_man.write_output_map(program_data.input_shapefile, program_data.output_shapefile)
-    ids = read_input_shapefile(program_data, shapefile_man)
-    read_output_shapefile(program_data, shapefile_man)
-    # shapefile_man.update_map_with_rssi_data(program_data.output_shapefile, 0, -60, 100)
-    # read_output_shapefile(program_data, shapefile_man)
+        shapefile_mgr.create_raw_shapefile_map_from_csv(program_data.input_csv, program_data.input_shapefile)
+    shapefile_mgr.create_output_shapefile_map_with_rssi_and_percent_values(program_data.input_shapefile, program_data.output_shapefile)
+    ids = shapefile_mgr.read_raw_shapefile_map(program_data.input_shapefile)
+    # display initial map
+    shapefile_mgr.read_output_shapefile_map_with_rssi_values(program_data.output_shapefile)
     perform_measurements(program_data, ids)
-    read_output_shapefile(program_data, shapefile_man)
+    # display map with the results of measurements
+    shapefile_mgr.read_output_shapefile_map_with_rssi_values(program_data.output_shapefile)
 
 
 if __name__ == '__main__':
--- shapefile_manager.py
+++ shapefile_manager.py
@@ -1,99 +1,97 @@
 from typing import List
 
+import logging
 import matplotlib.pyplot as plt
 import shapefile as shp  # type: ignore
-# import seaborn as sns  # type: ignore
 import pandas as pd
 
 
-class ShapefileManager:
+def create_raw_shapefile_map_from_csv(csv: str, input_shapefile: str):
+    data = pd.read_csv(csv)
+    with shp.Writer(input_shapefile, shapeType=shp.POLYGON) as w:
+        w.field('ID', 'N')
+        for i, polygon in data.iterrows():
+            w.poly([[
+                [polygon['x1'], polygon['y1']],
+                [polygon['x2'], polygon['y2']],
+                [polygon['x3'], polygon['y3']],
+                [polygon['x4'], polygon['y4']],
+                [polygon['x1'], polygon['y1']],
+            ]])
+            w.record(polygon['id'])
 
-    def write_input_map(self, csv: str, input_shapefile: str):
-        data = pd.read_csv(csv)
-        with shp.Writer(input_shapefile, shapeType=shp.POLYGON) as w:
-            w.field('ID', 'N')
-            for i, polygon in data.iterrows():
-                w.poly([[
-                    [polygon['x1'], polygon['y1']],
-                    [polygon['x2'], polygon['y2']],
-                    [polygon['x3'], polygon['y3']],
-                    [polygon['x4'], polygon['y4']],
-                    [polygon['x1'], polygon['y1']],
-                ]])
-                w.record(polygon['id'])
-
-    def write_output_map(self, input_shapefile: str, output_shapefile: str):
-        with shp.Reader(input_shapefile) as r:
-            with shp.Writer(output_shapefile, shapeType=shp.POLYGON) as w:
-                w.field('ID', 'N')
-                w.field('RSSI', 'N')
-                w.field('PERC', 'N')
-                for shaperec in r.iterShapeRecords():
-                    w.record(shaperec.record['ID'], 0, 0)
-                    w.shape(shaperec.shape)
-
-    def read_input(self, filename: str) -> List[int]:
-        with shp.Reader(filename) as sf:
-            ids = []
-            plt.figure()
-            for shape in sf.shapeRecords():
-                x = [i[0] for i in shape.shape.points[:]]
-                y = [i[1] for i in shape.shape.points[:]]
-                plt.plot(x, y)
-            for rec in sf.records():
-                ids.append(rec['ID'])
-            plt.show()
-            return ids
-
-    def read_output_rssi(self, filename: str):
-        def get_color(rssi):
-            if rssi < -90:
-                return 'blue'
-            elif rssi < -70:
-                return 'green'
-            elif rssi < -50:
-                return 'yellow'
-            else:
-                return 'red'
-        with shp.Reader(filename) as sf:
-            plt.figure()
-            for shaperec in sf.shapeRecords():
-                x = [i[0] for i in shaperec.shape.points[:]]
-                y = [i[1] for i in shaperec.shape.points[:]]
-                plt.fill_between(x, y, color=get_color(shaperec.record['RSSI']))
-                print(shaperec.record['ID'], shaperec.record['RSSI'], shaperec.record['PERC'])
-            plt.show()
-
-    def read_output_perc(self, filename: str):
-        def get_color(perc):
-            if perc < 40:
-                return 'blue'
-            elif perc < 60:
-                return 'green'
-            elif perc < 80:
-                return 'yellow'
-            else:
-                return 'red'
-        with shp.Reader(filename) as sf:
-            plt.figure()
-            for shaperec in sf.shapeRecords():
-                x = [i[0] for i in shaperec.shape.points[:]]
-                y = [i[1] for i in shaperec.shape.points[:]]
-                plt.fill_between(x, y, color=get_color(shaperec.record['PERC']))
-                print(shaperec.record['ID'], shaperec.record['RSSI'], shaperec.record['PERC'])
-            plt.show()
-
-    def update_map_with_rssi_data(self, shapefile: str, id: int, rssi: int, perc: int):
-        print(f"Update id {id} with RSSI {rssi} and {perc}%")
-        with shp.Reader(shapefile) as r:
-            records = list(r.iterShapeRecords())
-        with shp.Writer(shapefile, shapeType=shp.POLYGON) as w:
+def create_output_shapefile_map_with_rssi_and_percent_values(input_shapefile: str, output_shapefile: str):
+    with shp.Reader(input_shapefile) as r:
+        with shp.Writer(output_shapefile, shapeType=shp.POLYGON) as w:
             w.field('ID', 'N')
             w.field('RSSI', 'N')
             w.field('PERC', 'N')
-            for shaperec in records:
-                if shaperec.record['ID'] == id:
-                    w.record(id, rssi, perc)
-                else:
-                    w.record(shaperec.record['ID'], shaperec.record['RSSI'], shaperec.record['PERC'])
+            for shaperec in r.iterShapeRecords():
+                w.record(shaperec.record['ID'], 0, 0)
                 w.shape(shaperec.shape)
+
+def read_raw_shapefile_map(filename: str) -> List[int]:
+    with shp.Reader(filename) as sf:
+        ids = []
+        plt.figure()
+        for shape in sf.shapeRecords():
+            x = [i[0] for i in shape.shape.points[:]]
+            y = [i[1] for i in shape.shape.points[:]]
+            plt.plot(x, y)
+        for rec in sf.records():
+            ids.append(rec['ID'])
+        plt.show()
+        return ids
+
+def read_output_shapefile_map_with_rssi_values(filename: str):
+    def get_color(rssi):
+        if rssi < -90:
+            return 'blue'
+        elif rssi < -70:
+            return 'green'
+        elif rssi < -50:
+            return 'yellow'
+        else:
+            return 'red'
+    with shp.Reader(filename) as sf:
+        plt.figure()
+        for shaperec in sf.shapeRecords():
+            x = [i[0] for i in shaperec.shape.points[:]]
+            y = [i[1] for i in shaperec.shape.points[:]]
+            plt.fill_between(x, y, color=get_color(shaperec.record['RSSI']))
+            print(shaperec.record['ID'], shaperec.record['RSSI'], shaperec.record['PERC'])
+        plt.show()
+
+def read_output_shapefile_map_with_percent_values(filename: str):
+    def get_color(perc):
+        if perc < 40:
+            return 'blue'
+        elif perc < 60:
+            return 'green'
+        elif perc < 80:
+            return 'yellow'
+        else:
+            return 'red'
+    with shp.Reader(filename) as sf:
+        plt.figure()
+        for shaperec in sf.shapeRecords():
+            x = [i[0] for i in shaperec.shape.points[:]]
+            y = [i[1] for i in shaperec.shape.points[:]]
+            plt.fill_between(x, y, color=get_color(shaperec.record['PERC']))
+            print(shaperec.record['ID'], shaperec.record['RSSI'], shaperec.record['PERC'])
+        plt.show()
+
+def update_output_shapefile_map_with_rssi_values(shapefile: str, id: int, rssi: int, perc: int):
+    logging.debug(f"Update id {id} with RSSI {rssi} and {perc}%")
+    with shp.Reader(shapefile) as r:
+        records = list(r.iterShapeRecords())
+    with shp.Writer(shapefile, shapeType=shp.POLYGON) as w:
+        w.field('ID', 'N')
+        w.field('RSSI', 'N')
+        w.field('PERC', 'N')
+        for shaperec in records:
+            if shaperec.record['ID'] == id:
+                w.record(id, rssi, perc)
+            else:
+                w.record(shaperec.record['ID'], shaperec.record['RSSI'], shaperec.record['PERC'])
+            w.shape(shaperec.shape)
