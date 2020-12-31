1ea5704704092712d8a469c2dfb53175720f2563
Dominika Florczykowska <dflorczy@protonmail.com>
2020-12-31 09:35:20 +0000

    Create Gui class

--- gui.py
+++ gui.py
@@ -6,59 +6,54 @@ from shapefile_manager import ShapefileManager
 import tkinter as tk
 
 
-root = tk.Tk()
-canvas = None
-rects = []
+class Gui:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.canvas = None
+        self.polygons = []
+        self.shapefile_man = ShapefileManager()
+        self.shapefile_man.write_output_map('input_map', 'map')
+        tk.Button(self.root, text="Quit", command=self.root.quit).pack()
+        self.refresh_plot()
+        self.root.mainloop()
 
+    def refresh_plot(self):
+        self.polygons, fig = self.shapefile_man.read_output_rssi('map')
+        try:
+            self.canvas.get_tk_widget().pack_forget()
+        except AttributeError:
+            pass
+        self.canvas = FigureCanvasTkAgg(fig, master=self.root)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.canvas.mpl_connect('button_press_event', self.on_click)
 
-def refresh_plot(shapefile_man):
-    global rects
-    rects, fig = shapefile_man.read_output_rssi('map')
-    global canvas
-    try:
-        canvas.get_tk_widget().pack_forget()
-    except AttributeError:
-        pass
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-    canvas.mpl_connect('button_press_event', on_click)
+    def measure_point(self, id: int):
+        result = (-50, 90)
+        if result:
+            rssi, perc = result
+            self.shapefile_man.update_map_with_rssi_data('map', id, int(rssi), int(perc))
+            self.refresh_plot()
 
+    def find_point_id(self, x: int, y: int) -> Optional[int]:
+        for poly in self.polygons:
+            if x > min(poly.x) and x < max(poly.x) and y > min(poly.y) and y < max(poly.y):
+                return poly.id
+        return None
 
-def measure_point(id):
-    result = (-50, 90)
-    if result:
-        rssi, perc = result
-        shapefile_man = ShapefileManager()
-        shapefile_man.update_map_with_rssi_data('map', id, int(rssi), int(perc))
-        refresh_plot(shapefile_man)
-
-
-def find_point_id(x: int, y: int) -> Optional[int]:
-    for rect in rects:
-        print(rect)
-        if x > min(rect.x) and x < max(rect.x) and y > min(rect.y) and y < max(rect.y):
-            return rect.id
-    return None
-
-
-def on_click(event):
-    if event.inaxes is not None:
-        x = event.xdata
-        y = event.ydata
-        print(x, y)
-        id = find_point_id(x, y)
-        if id is not None:
-            measure_point(id)
-    else:
-        print('Clicked ouside axes bounds but inside plot window')
+    def on_click(self, event):
+        if event.inaxes is not None:
+            x = event.xdata
+            y = event.ydata
+            print(x, y)
+            id = self.find_point_id(x, y)
+            if id is not None:
+                self.measure_point(id)
+        else:
+            print('Clicked ouside axes bounds but inside plot window')
 
 
 def main():
-    shapefile_man = ShapefileManager()
-    shapefile_man.write_output_map('input_map', 'map')
-    tk.Button(root, text="Quit", command=root.quit).pack()
-    refresh_plot(shapefile_man)
-    root.mainloop()
+    Gui()
 
 
 if __name__ == '__main__':
