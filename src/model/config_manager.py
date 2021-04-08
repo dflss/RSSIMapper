ce67149a2b4f39106cb3e2fdcfdb256ff45edb44
Dominika Florczykowska <dflorczy@protonmail.com>
2021-04-08 19:19:59 +0200

    Fix flake errors

--- src/model/config_manager.py
+++ src/model/config_manager.py
@@ -5,7 +5,6 @@ from src.log import logger
 from src.model.program_data import ProgramData
 
 
-
 DEFAULT_PROGRAM_DATA = ProgramData('input_data/example-data.csv', 'input_data/input_map', 'output_data/output_results',
                                    'output_data/map', 'output_data/plot', '/dev/pts/2', 115200, 10, 100, 100)
 
