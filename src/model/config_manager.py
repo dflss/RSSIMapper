363066a63f18bf0cefe01dd332109c9011c0693d
Dominika Florczykowska <dflorczy@protonmail.com>
2021-04-26 10:06:19 +0200

    feat: move defaults to constants (#58)
    closes #56

--- src/model/config_manager.py
+++ src/model/config_manager.py
@@ -1,23 +1,9 @@
 import configparser
 import os
 
+from src.constants import DEFAULT_PROGRAM_DATA
 from src.log import logger
 from src.model.program_data import ProgramData
-
-
-DEFAULT_PROGRAM_DATA = ProgramData(
-    "input_data/example-data.csv",
-    "input_data/input_map",
-    "output_data/output_results",
-    "output_data/map",
-    "output_data/plot_rssi",
-    "output_data/plot_perc",
-    "/dev/pts/2",
-    115200,
-    10,
-    100,
-    100,
-)
 
 
 class ConfigManager:
